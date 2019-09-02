a8bbc15a4268c86ed4afc7232d41a017d953795c
Zplaninic <zvonimir.planinic@marfeel.com>
2019-09-02 21:47:37 +0200

    add strength exercise inputs

--- src/components/Strength.js
+++ src/components/Strength.js
@@ -1,11 +1,14 @@
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
+import StrengthTrainingForm from './StrengthTrainingForm';
 
 class Strength extends Component {
 	render() {
 		return (
 			<div className="strength-training">
 				<h1>Strength training</h1>
+				<StrengthTrainingForm/>
+
 			</div>
 		);
 	}
