737d2bc40fa2fb925944c79c157ded9990329304
Zvonimir Planinic <zvonimir.planinic@Zvonimirs-MacBook-Air.local>
2019-11-12 10:53:55 +0100

    update eslint

--- src/components/StrengthExercise.js
+++ src/components/StrengthExercise.js
@@ -1,22 +1,22 @@
 /* eslint-disable class-methods-use-this */
-import React, { Component } from 'react';
-import { Ul, Li} from '../css/elements/Lists';
+import React, { Component } from "react";
+import { Ul, Li } from "../css/elements/Lists";
 
 class StrengthExercise extends Component {
-	render() {
-		const { name, type, sets, reps, rest, date} = this.props.exerciseDetails;
+  render() {
+    const { name, type, sets, reps, rest, date } = this.props.exerciseDetails;
 
-		return (
-			<Ul className="exercise">
-				<Li>{date}</Li>
-				<Li>{name}</Li>
-				<Li>{type}</Li>
-				<Li>Sets: {sets}</Li>
-				<Li>Reps: {reps}</Li>
-				<Li>Rest: {rest} s</Li>
-			</Ul>
-		);
-	}
+    return (
+      <Ul className="exercise">
+        <Li>{date}</Li>
+        <Li>{name}</Li>
+        <Li>{type}</Li>
+        <Li>Sets: {sets}</Li>
+        <Li>Reps: {reps}</Li>
+        <Li>Rest: {rest} s</Li>
+      </Ul>
+    );
+  }
 }
 
 export default StrengthExercise;
