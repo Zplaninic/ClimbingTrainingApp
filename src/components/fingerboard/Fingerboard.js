6a68995ebf6f1a0d6a54067f777a05b35c408888
Zplaninic <zvonimir.planinic@gmail.com>
2020-03-17 21:04:58 +0100

    change fingerboard component to hooks

--- src/components/fingerboard/Fingerboard.js
+++ src/components/fingerboard/Fingerboard.js
@@ -1,44 +1,32 @@
 /* eslint-disable class-methods-use-this */
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import FingerboardForm from "./FingerBoardForm";
-import Countdown from "../tools/Countdown";
 import base from "../../base";
 
-class Fingerboard extends Component {
-  state = {
-    sessions: {}
+const Fingerboard = ({ userId }) => {
+  const [sessions, setSession] = useState([]);
+
+  // console.log(sessions);
+  // useEffect(() => {
+  //   const ref = base.syncState(`${this.props.userId}/fingerboard`, {
+  //     context: this,
+  //     state: "sessions"
+  //   });
+  // });
+
+  // componentWillUnmount() {
+  //   base.removeBinding(this.ref);
+  // }
+  const addSession = session => {
+    setSession([...sessions, { [`Session${Date.now()}`]: session }]);
   };
 
-  componentDidMount() {
-    this.ref = base.syncState(`${this.props.userId}/fingerboard`, {
-      context: this,
-      state: "sessions"
-    });
-  }
-
-  componentWillUnmount() {
-    base.removeBinding(this.ref);
-  }
-
-  addSession = session => {
-    const sessions = { ...this.state.sessions };
-
-    sessions[`Session${Date.now()}`] = session;
-
-    this.setState({
-      sessions: sessions
-    });
-  };
-
-  render() {
-    return (
-      <div className="fingerboard-traning">
-        <h1>Fingerboard traning</h1>
-        <FingerboardForm addSession={this.addSession} />
-        <Countdown />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="fingerboard-traning">
+      <h1>Fingerboard traning</h1>
+      <FingerboardForm addSession={addSession} />
+    </div>
+  );
+};
 
 export default Fingerboard;
