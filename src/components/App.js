e31acd77ab0cecffe77cc257cb0c1c3041413053
Zplaninic <zvonimir.planinic@gmail.com>
2020-05-04 00:04:30 +0200

    modify app component to use with cookies and axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./private/PrivateRoute";
 import Climbing from "./climbing/Climbing";
 import Strength from "./strength/Strength";
 import Fingerboard from "./fingerboard/Fingerboard";
@@ -7,8 +8,6 @@ import NotFound from "./basic/NotFound";
 import Home from "./Home";
 import Analysis from "./analysis/Analysis";
 import Admin from "./private/Admin";
-import { AuthContext } from "./../context/auth";
-import PrivateRoute from "./private/PrivateRoute";
 import Login from "./authentication/Login";
 import Signup from "./authentication/Signup";
 import Header from "./../components/basic/Header";
@@ -16,75 +15,25 @@ import { GlobalStyle } from "./../css/style";
 import Footer from "./../components/basic/Footer";
 
 const App = () => {
-  const [isLoggedIn, setLoggedIn] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const readSession = () => {
-    const user = window.sessionStorage.getItem("login");
-    let token = JSON.parse(user);
-
-    if (token) {
-      setLoggedIn(true);
-    } else {
-      setIsLoading(false);
-    }
-  };
-  useEffect(() => {
-    readSession();
-  }, []);
   return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn,
-        setLoggedIn,
-        isLoading,
-        setIsLoading
-      }}
-    >
-      <GlobalStyle></GlobalStyle>
-      <Header></Header>
-      <Router>
-        <React.Fragment>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <PrivateRoute
-              path="/climbing"
-              component={Climbing}
-              isLoggedIn={isLoggedIn}
-              isLoading={isLoading}
-            />
-            <PrivateRoute
-              path="/strength"
-              component={Strength}
-              isLoggedIn={isLoggedIn}
-              isLoading={isLoading}
-            />
-            <PrivateRoute
-              path="/fingerboard"
-              component={Fingerboard}
-              isLoggedIn={isLoggedIn}
-              isLoading={isLoading}
-            />
-            <PrivateRoute
-              path="/analysis"
-              component={Analysis}
-              isLoggedIn={isLoggedIn}
-              isLoading={isLoading}
-            />
-            <PrivateRoute
-              path="/admin"
-              component={Admin}
-              isLoggedIn={isLoggedIn}
-              isLoading={isLoading}
-            />
-            <Route path="/*" component={NotFound} />
-          </Switch>
-        </React.Fragment>
-      </Router>
-      <Footer></Footer>
-    </AuthContext.Provider>
+    <Router>
+      <React.Fragment>
+        <GlobalStyle></GlobalStyle>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <PrivateRoute exact path="/climbing" component={Climbing} />
+          <PrivateRoute exact path="/strength" component={Strength} />
+          <PrivateRoute exact path="/fingerboard" component={Fingerboard} />
+          <PrivateRoute exact path="/analysis" component={Analysis} />
+          <PrivateRoute exact path="/admin" component={Admin} />
+          <Route path="/*" component={NotFound} />
+        </Switch>
+        <Footer></Footer>
+      </React.Fragment>
+    </Router>
   );
 };
 
