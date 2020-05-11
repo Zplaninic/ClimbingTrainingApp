ec39e19308362be5c34fb698fc08aeb89b5ba80d
Zplaninic <zvonimir.planinic@gmail.com>
2020-05-11 12:35:13 +0200

    change headers and footers

--- src/components/basic/Footer.js
+++ src/components/basic/Footer.js
@@ -3,17 +3,27 @@ import styled from "styled-components";
 
 const Footer = () => (
   <FooterTag>
-    <p>Copyright 2020</p>
+    <div>Copyright 2020</div>
+    <a href="https://github.com/Zplaninic/ClimbingTrainingApp">
+      Check the project at Github
+    </a>
   </FooterTag>
 );
 
 const FooterTag = styled.footer`
-  text-align: center;
-  background-color: #e7b060;
-  font-size: 1rem;
-  letter-spacing: 1px;
-  color: #ae493f;
-  padding: 10px;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: red;
+  color: white;
+  margin-left: 300px;
+  font-size: 14px;
+  padding: 5px;
+
+  div {
+    padding-bottom: 5px;
+  }
 `;
 
 export default Footer;
