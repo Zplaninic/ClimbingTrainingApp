a8794872b94e53b41c7a603699fa9cc75ed679e0
Zplaninic <zvonimir.planinic@gmail.com>
2020-05-14 15:05:46 +0200

    mobile design home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./private/PrivateRoute";
 import Climbing from "./climbing/Climbing";
@@ -12,20 +12,52 @@ import Login from "./authentication/Login";
 import Signup from "./authentication/Signup";
 import Top from "./basic/Top";
 import { GlobalStyle } from "./../css/style";
+import { useOnClickOutside } from "./hooks/useOnClickOutside";
 
 const App = () => {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
     <Router>
       <React.Fragment>
-        <GlobalStyle></GlobalStyle>
-        <Top />
+        <GlobalStyle />
+        <Top open={open} setOpen={setOpen} />
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <Home {...props} open={open} setOpen={setOpen} node={node} />
+            )}
+          />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
-          <PrivateRoute exact path="/climbing" component={Climbing} />
-          <PrivateRoute exact path="/strength" component={Strength} />
-          <PrivateRoute exact path="/fingerboard" component={Fingerboard} />
+          <PrivateRoute
+            exact
+            path="/climbing"
+            component={Climbing}
+            open={open}
+            setOpen={setOpen}
+            node={node}
+          />
+          <PrivateRoute
+            exact
+            path="/strength"
+            component={Strength}
+            open={open}
+            setOpen={setOpen}
+            node={node}
+          />
+          <PrivateRoute
+            exact
+            path="/fingerboard"
+            component={Fingerboard}
+            open={open}
+            setOpen={setOpen}
+            node={node}
+          />
           <PrivateRoute exact path="/analysis" component={Analysis} />
           <PrivateRoute exact path="/admin" component={Admin} />
           <Route path="/*" component={NotFound} />
